52c816151233d36ce47260ee408b58ea8bb37cea
Penguin Jackson <99298635+coderforu@users.noreply.github.com>
2023-04-22 15:16:32 -0400

    Update EnumOptions.java

--- src/main/java/net/minecraft/src/EnumOptions.java
+++ src/main/java/net/minecraft/src/EnumOptions.java
@@ -1,4 +1,5 @@
- CHAT_HEIGHT_FOCUSED("options.chat.height.focused", true, false), CHAT_HEIGHT_UNFOCUSED("options.chat.height.unfocused", true, false), CHUNK_UPDATES("options.chunkUpdates", false, false), ADDERALL("options.adderall", false, true),
+@@ -11,7 +11,7 @@ public enum EnumOptions {
+    CHAT_HEIGHT_FOCUSED("options.chat.height.focused", true, false), CHAT_HEIGHT_UNFOCUSED("options.chat.height.unfocused", true, false), CHUNK_UPDATES("options.chunkUpdates", false, false), ADDERALL("options.adderall", false, true),
     /* Precision Client settings */
     DYNAMIC_FOV("prcmenu.options.dynamicFOV", false, true), FULLBRIGHT("prcmenu.mods.fullBright", false, true), KEYSTROKES("prcmenu.mods.keyStrokes", false, true), VIGNETTE("prcmenu.options.vignette", false, true),
     RAIN("prcmenu.options.rain", false, true);
